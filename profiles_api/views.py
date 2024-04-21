6352bf61c63cba70d14b8e16b41552522f3d511e
Konstantin711 <rc.work.yarm@gmail.com>
2024-04-21 14:32:35 +0300

    first view and first url

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+
+class HelloApiView(APIView):
+    """Test API View"""
+
+    def get(self, request, format=None):
+        """Returns a list of APIView features"""
+
+        an_apiview = [
+            'Uses HTTP methods as functions (get, post, put, path)',
+            'Is similar to a traditional Django View',
+            'Gives you xthe most control over your application logic',
+            'Is mapped manually to URLs'
+        ]
+
+        return Response({'message': 'Response', 'an_apiview': an_apiview})
--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
 
     'profiles_api',
     
@@ -126,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
