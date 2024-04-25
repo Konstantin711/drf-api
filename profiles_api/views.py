fdd1ec0ba55ad06e69c11acfe19a986188ac9969
Konstantin711 <rc.work.yarm@gmail.com>
2024-04-25 18:11:19 +0300

    viewset crud created

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import viewsets
 
 from rest_framework import status
 from profiles_api import serializers
@@ -36,4 +37,84 @@ class HelloApiView(APIView):
         return Response(
             serializer.errors,
             status=status.HTTP_400_BAD_REQUEST
+        )
+    
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet"""
+
+    serializer_class = serializers.HelloSerializer 
+
+    def list(self, request):
+        """Return hello message"""
+        a_viewset = [
+            'Uses actions (list, create, retrieve, update, partial_update, destroy)',
+            'Automatically maps to URLs using Routers',
+            'Provides more functionality with less code'
+        ]
+
+        return Response(
+            dict(
+                message='Hello',
+                a_viewset=a_viewset
+            )
+        )
+    
+    def create(self, request):
+        """Create new hello message"""
+
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+
+            return Response({'message': message})
+        
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    
+    def retrieve(self, request, pk=None):
+        """Handle getting an obj by its ID(pk)"""
+
+        return Response(
+            dict(
+                view_method='retrieve() - get specified obj',
+                http_method=request.method
+            )
+        )
+    
+    def update(self, request, pk=None):
+        """Handle update of obj by its ID(pk)"""
+
+        return Response(
+            dict(
+                view_method='update() - get specified obj',
+                http_method=request.method
+            )
+        )
+    
+
+    def partial_update(self, request, pk=None):
+        """Handle update of obj by its ID(pk)"""
+
+        return Response(
+            dict(
+                view_method='partial_update() - update obj partially',
+                http_method=request.method
+            )
+        )
+    
+
+    def delete(self, request, pk=None):
+        """Handle update of obj by its ID(pk)"""
+
+        return Response(
+            dict(
+                view_method='delete() - update obj partially',
+                http_method=request.method
+            )
         )
